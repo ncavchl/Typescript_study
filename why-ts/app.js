d78f5bd1cd0f1642499c56b9e67bce72ec2b58b8
ncavchl <ncavchl@ajou.ac.kr>
2022-12-08 20:19:18 +0900

    ts js 변환

--- why-ts/app.js
+++ why-ts/app.js
@@ -1,16 +1,36 @@
 // api url
-var url = 'https://jsonplaceholder.typicode.com/users/1';
+var url = "https://jsonplaceholder.typicode.com/users/1";
 
 // dom
-var username = document.querySelector('#username');
-var email = document.querySelector('#email');
-var address = document.querySelector('#address');
+var username = document.querySelector("#username");
+var email = document.querySelector("#email");
+var address = document.querySelector("#address");
 
 // user data
 var user = {};
 
+/**
+ * @typedef {object} User
+ * @property {string} name
+ * @property {string} email
+ * @property {string} address
+ *
+ */
+
+/**
+ *
+ * @returns {Promise<User>}
+ */
+
+function fetchUser() {
+  return axios.get(url);
+}
+fetchUser().then(function (response) {
+  response.address;
+});
+
 function startApp() {
-  axios
+  fetchUser
     .get(url)
     .then(function (response) {
       console.log(response);
